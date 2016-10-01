f6390260c28b77b2b44044ce36d1e1b4208aee68
simon <wsj_simon@163.com>
2016-10-02 01:40:58 +0800

    图片库完全的分离，文字变缩略图
    使用addLoadEvent，insertAfter了；html中的占位图及描述文字通过js写入，减少了标记；

--- js图片库/scripts/showPic.js
+++ js图片库/scripts/showPic.js
@@ -1,34 +1,76 @@
-function prepareGallery(){
+function addLoadEvent(func){
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function'){//如果处理函数未绑定任何函数，添加
+    window.onload = func;
+  }else {//如果已经有绑定的，在末尾追加
+    window.onload = function(){
+      oldonload();
+      func();
+    }
+  }
+}
+
+function insertAfter(newElement,targetElement){//已有元素后插入新
+  var parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  }else {
+    parent.insertBefore(newElement,targetElement.nextSibling);
+  }
+}
+
+function preparePlaceholder(){
+	if (!document.createElement) return false;
+	if (!document.createTextNode) return false;
 	if (!document.getElementById) return false;
-	if (!document.getElementsByTagName) return false;
 	if (!document.getElementById("imagegallery")) return false;
-	var gallery=document.getElementById("imagegallery");
-	var links=gallery.getElementsByTagName("a");
-	for(var i=0;i<links.length;i++){
-		links[i].onclick=function(){
-			return showPic(this) ? false : true;
-			//?是三元操作符，variable = condition ? if true : if false;相当于if/else语句
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/team.jpg");
+	placeholder.setAttribute("alt","my image gallery");
+	var description = document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("Choose an image");
+	description.appendChild(desctext);
+	var gallery = document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+
+function prepareGallery(){
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for ( var i=0; i<links.length; i++){
+		links[i].onclick = function(){
+			return showPic(this);
 		}
+		links[i].onkeypress = links[i].onclick;
 	}
 }
-window.onload=prepareGallery;
+
 function showPic(whichpic){
-	//假设id为placeholder不存在
 	if (!document.getElementById("placeholder")) return false;
 	var placeholder=document.getElementById("placeholder");
 	var source = whichpic.getAttribute("href");
-	//假设placeholder的节点名称不是img，nodeName返回的是大写
-	if (placeholder.nodeName != "IMG") return false;
 	placeholder.setAttribute("src",source);
-	//假设id为description不存在
-	if (document.getElementById("description")) {
-		//假设whichpic的tittle属性不存在
-		var text = whichpic.getAttribute("tittle") ? whichpic.getAttribute("tittle") : "";
-		var description=document.getElementById("description");
-		//如果description的第一个子节点的类型是3，即文本节点？ 1是元素，2是属性。
-		if (description.firstChild.nodeType == 3){
-			description.firstChild.nodeValue=text;
-		}
+	if (!document.getElementById("description")) return false;
+	if (whichpic.getAttribute("tittle")){
+		var text = whichpic.getAttribute("tittle");
+	}else { var text = "";
 	}
-	return true;
+	var description=document.getElementById("description");
+	if (description.firstChild.nodeType == 3){
+		description.firstChild.nodeValue=text;
+	}
+	return false;
 }
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+
+
+
+
