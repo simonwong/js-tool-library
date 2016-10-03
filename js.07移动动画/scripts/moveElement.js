d1d47b147015fd9f32eea8645c61a0e977f4539b
simon <wsj_simon@163.com>
2016-10-03 23:46:33 +0800

    移动动画
    W3C在（Web内容可访问性指南）7.2节给出建议，除非允许用户“冻结”移动的内容，否则应该避免

---
+++ js.07移动动画/scripts/moveElement.js
@@ -0,0 +1,47 @@
+function addLoadEvent(func){
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function'){
+    window.onload = func;
+  }else {
+    window.onload = function(){
+      oldonload();
+      func();
+    }
+  }
+}
+function moveElement(elementID,final_x,final_y,interval){
+	if (!document.getElementById) return false;
+	if (!document.getElementById(elementID)) return false;
+	var elem = document.getElementById(elementID);
+	var xpos = parseInt(elem.style.left);
+	var ypos = parseInt(elem.style.top);
+	if (xpos == final_x && ypos == final_y){
+		return true;
+	}
+	if (xpos < final_x){
+		xpos++;
+	}
+	if (xpos > final_x){
+		xpos--;
+	}
+	if (ypos < final_y){
+		ypos++;
+	}if (ypos > final_y){
+		ypos--;
+	}
+	elem.style.left = xpos + "px";
+	elem.style.top = ypos + "px";
+	var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+	movement = setTimeout(repeat,interval);
+}
+function positionMessage(){
+	if (!document.getElementById) return false;
+	if (!document.getElementById("message")) return false;
+	var elem = document.getElementById("message");
+	elem.style.position = "absolute";
+	elem.style.left = "50px";
+	elem.style.top = "100px";
+	moveElement("message",200,100,10);
+}
+addLoadEvent(positionMessage);
+//W3C在（Web内容可访问性指南）7.2节给出建议，除非允许用户“冻结”移动的内容，否则应该避免
