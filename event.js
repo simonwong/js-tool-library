edd2a66f77d88df7bea1adb063300271f849bfdd
simon <wsj_simon@163.com>
2016-10-03 23:58:30 +0800

    新增添加class，移动动画效果

--- event.js
+++ event.js
@@ -40,7 +40,46 @@ function styleElementSibilings(tag.theclass){
 		addClass(elem,theclass);
 	}
 }
-
+//移动效果，向左右上下移动
+function moveElement(elementID,final_x,final_y,interval){
+  if (!document.getElementById) return false;
+	if (!document.getElementById(elementID)) return false;
+	var elem = document.getElementById(elementID);
+  if (elem.movement) {
+    clearTimeout(elem.movement);
+  }
+  if (!elem.style.left) {
+    elem.style.left = "0px";
+  }
+  if (!elem.style.top) {
+    elem.style.top = "0px";
+  }
+	var xpos = parseInt(elem.style.left);
+	var ypos = parseInt(elem.style.top);
+  var dist = 0;
+	if (xpos == final_x && ypos == final_y){
+		return true;
+	}
+	if (xpos < final_x){
+    dist = Math.ceil((final_x - xpos)/10);
+		xpos = xpos + dist;
+	}
+	if (xpos > final_x){
+    dist = Math.ceil((xpos - final_x)/10);
+		xpos = xpos - dist;
+	}
+	if (ypos < final_y){
+    dist = Math.ceil((final_y - ypos)/10);
+    ypos = ypos + dist;
+	}if (ypos > final_y){
+    dist = Math.ceil((ypos - final_y)/10);
+    ypos = ypos - dist;
+	}
+	elem.style.left = xpos + "px";
+	elem.style.top = ypos + "px";
+	var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+	elem.movement = setTimeout(repeat,interval);
+}
 
 
 
