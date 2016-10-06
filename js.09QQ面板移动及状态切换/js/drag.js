c5d59e3d719bc04123e6353c7ad300b08a6e6aae
simon <wsj_simon@163.com>
2016-10-06 23:46:52 +0800

    更新
    面板关闭；状态切换

--- js.09QQ面板移动及状态切换/js/drag.js
+++ js.09QQ面板移动及状态切换/js/drag.js
@@ -21,12 +21,11 @@ function getByClass(className,parent){
   }
   return eles;
 }
-
+//拖曳
 function drag(){
   var oTitle = getByClass("login_logo_webqq","loginPanel")[0];
   // 该区域鼠标按下是的动作
   oTitle.onmousedown=fnDown;
-
 }
 // 按下的动作
 function fnDown(event){
@@ -53,7 +52,7 @@ function fnMove(e,X,Y){
       winH = document.documentElement.clientHeight || document.body.clientHeight,
       maxW = winW - oDrag.offsetWidth - 10,
       maxH = winH - oDrag.offsetHeight;
-
+  //设置可移动最大范围
   if (mX < 0){
     mX = 0
   }else if (mX > maxW){
@@ -67,4 +66,63 @@ function fnMove(e,X,Y){
   oDrag.style.left = mX+"px";
   oDrag.style.top = mY+"px";
 }
-addLoadEvent(drag)
+
+//切换状态
+function loginState(){
+  var state = document.getElementById("loginState");
+  state.onclick = stateShow;
+}
+function stateShow(event){
+  //阻止事件冒泡为了在空白处onclick，也能隐藏
+  event = event || window.event;
+      if (event.stopPropagation) {
+            event.stopPropagation();
+          }else{
+            event.cancleBuble();
+          }
+  var statePanel = document.getElementById("loginStatePanel");
+  //显示状态选择
+  statePanel.style.display = "block";
+  var panelList = getByClass("statePanel_li","loginStatePanel");
+  for(var i=0 ; i<panelList.length; i++){
+    //鼠标经过
+    panelList[i].onmouseover = function(){
+      this.style.backgroundColor = "#ccc";
+    }
+    //鼠标离开
+    panelList[i].onmouseout = function(){
+      this.style.backgroundColor = "#fff";
+    }
+    //鼠标按下
+    panelList[i].onclick = function(event){
+      //此处再点击时none，冒泡到外面，外面已经显示block，导致无法隐藏，所以要阻止事件冒泡
+      event = event || window.event;
+      if (event.stopPropagation) {
+            event.stopPropagation();
+          }else{
+            event.cancleBuble();
+          }
+      statePanel.style.display = "none";
+      var showPic  = document.getElementById("loginStateShow"),
+          showTxt =document.getElementById("login2qq_state_txt"),
+          id = this.id
+      showPic.className = " ";
+      showPic.className = "login-state-show " + id;
+      showTxt.innerHTML = getByClass("stateSelect_text",id)[0].innerHTML;
+    }
+    //在外部点击时，也能隐藏
+    window.onclick = function(){
+      statePanel.style.display = "none";
+    }
+  }
+}
+function closeBox(){
+  var close = document.getElementById("ui_boxyClose");
+  close.onclick = function(){
+    var loginPanel = document.getElementById("loginPanel");
+    loginPanel.style.display = "none";
+  }
+}
+addLoadEvent(drag);
+addLoadEvent(loginState);
+addLoadEvent(closeBox);
